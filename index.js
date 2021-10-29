0cf7aa11d82054f3c7d43003a1a61e1dd818327a
Md Ashikul Hossain Shfol <63502805+shofol1@users.noreply.github.com>
2021-10-29 22:01:24 +0600

    mongodb-connected

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleWire
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5va8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
